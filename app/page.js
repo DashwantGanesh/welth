442c69b8225606a6ec7b9b7e905c62e4e7911c9f
Ganesh Dashwant <ganeshdashwant04@gmail.com>
2025-07-16 20:21:32 +0530

    Inital Commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Divide } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return <div><Button variant="destructive">hey there</Button></div>
+}
